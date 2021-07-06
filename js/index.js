fde0f45d9209ec235de8318736f494baccad4f1e
Willis Li <li.willis95@gmail.com>
2021-07-06 15:53:56 -0700

    completed mvp

--- js/index.js
+++ js/index.js
@@ -1,1 +1,66 @@
-// Your code goes here
+const anchors = document.querySelector(".nav");
+anchors.addEventListener("mouseover", (event) => {
+    event.target.style.color = "green";
+    event.preventDefault;
+});
+
+anchors.addEventListener("mouseout", (event) => {
+    event.target.style.color = "black";
+    event.preventDefault;
+});
+
+const buttons = document.querySelectorAll(".btn");
+buttons.forEach(function(button) {
+    button.addEventListener("click", (event) => {
+        event.target.style.backgroundColor = "silver";
+    })
+});
+
+function top(event) {
+    if (event.keyCode === 87)
+        window.scrollTo(0,0);
+}
+
+function down(event) {
+    if (event.keyCode === 83)
+        window.scrollTo(0, document.body.scrollHeight);
+}
+
+document.addEventListener("keydown", top);
+document.addEventListener("keyup", down);
+
+const ps = document.querySelectorAll("p");
+ps.forEach(function(p) {
+    p.addEventListener("mouseup", (event) => {
+        event.target.style.color = "pink";
+    });
+});
+
+const bus = document.querySelectorAll("img");
+
+bus[1].addEventListener("dblclick", (event) => {
+    event.target.style.width = "20vw";
+});
+
+bus[2].addEventListener("dblclick", (event) => {
+    event.target.style.width = "20vw";
+});
+
+const scroll = document.querySelector("body");
+scroll.addEventListener("wheel", (event) => {
+    event.target.style.backgroundColor = "gray";
+})
+
+const scrollHead = document.querySelector("header");
+scrollHead.addEventListener("wheel", (event) => {
+    event.target.style.backgroundColor = "lightgray";
+    event.stopPropagation();
+})
+
+document.addEventListener("copy", (event) => {
+    console.log("You copied something!")
+});
+
+document.addEventListener('fullscreenchange', (event) => {
+    console.log("You just changed your screen size!");
+});
